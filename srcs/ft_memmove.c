6a0563776076a666d98471098f26c4cedab5e4f0
jguyon <jguyon@student.42.fr>
2016-12-28 15:19:05 +0100

    Optimize ft_memmove

--- srcs/ft_memmove.c
+++ srcs/ft_memmove.c
@@ -6,27 +6,58 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 17:24:14 by jguyon            #+#    #+#             */
-/*   Updated: 2016/11/07 17:34:03 by jguyon           ###   ########.fr       */
+/*   Updated: 2016/12/28 15:16:49 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+static void	*cpy_left(void *dst, const void *src, size_t n)
 {
-	size_t			i;
-	unsigned char	*udst;
-	unsigned char	*usrc;
+	size_t	align;
+	size_t	i;
 
-	if (dst > src)
+	align = n & 7;
+	i = 0;
+	while (i < align)
 	{
-		udst = (unsigned char *)dst;
-		usrc = (unsigned char *)src;
-		i = len;
-		while (i-- > 0)
-			udst[i] = usrc[i];
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+		++i;
 	}
-	else
-		ft_memcpy(dst, src, len);
+	while (i < n)
+	{
+		*((uint64_t *)((unsigned char *)dst + i))
+			= *((uint64_t *)((unsigned char *)src + i));
+		i += 8;
+	}
 	return (dst);
+}
+
+static void	*cpy_right(void *dst, const void *src, size_t n)
+{
+	size_t	align;
+	size_t	i;
+
+	align = n - (n & 7);
+	i = n;
+	while (i > align)
+	{
+		--i;
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+	}
+	while (i)
+	{
+		i -= 8;
+		*((uint64_t *)((unsigned char *)dst + i))
+			= *((uint64_t *)((unsigned char *)src + i));
+	}
+	return (dst);
+}
+
+void		*ft_memmove(void *dst, const void *src, size_t len)
+{
+	if (dst > src)
+		return (cpy_right(dst, src, len));
+	else
+		return (cpy_left(dst, src, len));
 }
--- tests/test_strings.c
+++ tests/test_strings.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/27 16:59:10 by jguyon            #+#    #+#             */
-/*   Updated: 2016/12/28 12:51:12 by jguyon           ###   ########.fr       */
+/*   Updated: 2016/12/28 15:10:55 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,10 +70,24 @@ TFT_TEST(test_memccpy)
 	TFT_ASSERT(strcmp(mem_ft, mem_lc) == 0);
 }
 
+TFT_TEST(test_memmove)
+{
+	char	mem_ft[256] = {0};
+	char	mem_lc[256] = {0};
+
+	strcpy(mem_ft, "hello world hello world hello world");
+	strcpy(mem_lc, "hello world hello world hello world");
+	TFT_ASSERT(strcmp(ft_memmove(mem_ft, mem_ft + 3, 200),
+					  memmove(mem_lc, mem_lc + 3, 200)) == 0);
+	TFT_ASSERT(strcmp(ft_memmove(mem_ft + 3, mem_ft, 200),
+					  memmove(mem_lc + 3, mem_lc, 200)) == 0);
+}
+
 void	test_strings(void)
 {
 	TFT_RUN(test_memset);
 	TFT_RUN(test_memcpy);
 	TFT_RUN(test_strlen);
 	TFT_RUN(test_memccpy);
+	TFT_RUN(test_memmove);
 }
