a08d2d1b223b50e514f6f0eb6d0fa028d9e31291
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-01-08 12:49:47 +0100

    Document formatted output functions

--- includes/libft/ft_printf.h
+++ includes/libft/ft_printf.h
@@ -6,21 +6,59 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/02 03:31:40 by jguyon            #+#    #+#             */
-/*   Updated: 2017/01/02 03:32:37 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/01/08 12:49:33 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
+/*
+** Formatted output functions
+**
+** These functions return the number of bytes written if successful,
+** a negative number otherwise.
+*/
+
 # include <stdarg.h>
 # include "libft/ft_streams.h"
 
+/*
+** ft_printf - print formatted string to stdout
+** @format: format to print
+** @rest: arguments referenced by @format
+*/
 int		ft_printf(const char *format, ...);
+
+/*
+** ft_vprintf - va_list variant of ft_printf
+*/
 int		ft_vprintf(const char *format, va_list ap);
+
+/*
+** ft_dprintf - print formatted string to a file
+** @fd: file descriptor
+** @format: format to print
+** @rest: arguments referenced by @format
+*/
 int		ft_dprintf(int fd, const char *format, ...);
+
+/*
+** ft_vdprintf - va_list variant of ft_dprintf
+*/
 int		ft_vdprintf(int fd, const char *format, va_list ap);
+
+/*
+** ft_fprintf - print formatted string to a stream
+** @stream: stream to print to
+** @format: format to print
+** @rest: arguments referenced by @format
+*/
 int		ft_fprintf(t_stream *stream, const char *format, ...);
+
+/*
+** ft_vfprintf - va_list variant of ft_fprintf
+*/
 int		ft_vfprintf(t_stream *stream, const char *format, va_list ap);
 
 #endif
