578fa13e7fc1470681efba9304bd95ed189b6d7b
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-01-09 16:17:45 +0100

    Use less memory for convert functions array

--- srcs/printf/pf_convert.c
+++ srcs/printf/pf_convert.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/08 17:04:02 by jguyon            #+#    #+#             */
-/*   Updated: 2017/01/09 15:40:45 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/01/09 16:16:24 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,27 @@
 #include "priv/pf_convert.h"
 #include "priv/pf_parse.h"
 
-static t_conv	*g_convs[CHAR_MAX + 1] = {
-	[PF_CHAR_SPEC] = &pf_convert_char,
-	[PF_STRING_SPEC] = &pf_convert_str,
-	[PF_INT_SPEC] = &pf_convert_int,
-	[PF_ALTINT_SPEC] = &pf_convert_int,
-	[PF_UINT_SPEC] = &pf_convert_uint,
-	[PF_OCT_SPEC] = &pf_convert_uint,
-	[PF_HEXLO_SPEC] = &pf_convert_uint,
-	[PF_HEXUP_SPEC] = &pf_convert_uint,
-	[PF_ADDR_SPEC] = &pf_convert_uint,
-	[PF_PTR_SPEC] = &pf_convert_ptr,
+#define SPECMIN ' '
+#define SPECMAX '~'
+#define SPECTOI(s) (s - SPECMIN)
+
+static t_conv	*g_convs[SPECMAX - SPECMIN + 1] = {
+	[SPECTOI(PF_CHAR_SPEC)] = &pf_convert_char,
+	[SPECTOI(PF_STRING_SPEC)] = &pf_convert_str,
+	[SPECTOI(PF_INT_SPEC)] = &pf_convert_int,
+	[SPECTOI(PF_ALTINT_SPEC)] = &pf_convert_int,
+	[SPECTOI(PF_UINT_SPEC)] = &pf_convert_uint,
+	[SPECTOI(PF_OCT_SPEC)] = &pf_convert_uint,
+	[SPECTOI(PF_HEXLO_SPEC)] = &pf_convert_uint,
+	[SPECTOI(PF_HEXUP_SPEC)] = &pf_convert_uint,
+	[SPECTOI(PF_ADDR_SPEC)] = &pf_convert_uint,
+	[SPECTOI(PF_PTR_SPEC)] = &pf_convert_ptr,
 };
 
 int				pf_convert(t_stream *stream, t_pf_info *info, va_list args)
 {
-	if (g_convs[(size_t)info->spec])
-		return (g_convs[(size_t)info->spec](stream, info, args));
+	if (info->spec >= SPECMIN && info->spec <= SPECMAX
+		&& g_convs[SPECTOI(info->spec)])
+		return (g_convs[SPECTOI(info->spec)](stream, info, args));
 	return (-1);
 }
