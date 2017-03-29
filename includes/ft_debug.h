8ca70444dfb62744962afea1d6a37aec5b640361
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-03-29 15:29:11 +0200

    Add feature flag to activate debug utils
    Compilation needs to use -DFT_FEATURE_DEBUG as an argument
    for the debug utils not to be skipped.

--- includes/ft_debug.h
+++ includes/ft_debug.h
@@ -6,12 +6,22 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/29 11:42:02 by jguyon            #+#    #+#             */
-/*   Updated: 2017/03/29 14:53:30 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/03/29 15:27:09 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_DEBUG_H
 # define FT_DEBUG_H
+
+/*
+** Debug utils
+**
+** FT_FEATURE_DEBUG needs to be previously defined for the macros not
+** to be skipped.
+** The easiest way to do that is to compile the lib with
+** `make CPPFLAGS=-DFT_FEATURE_DEBUG` and compile your program
+** with `clang -DFT_FEATURE_DEBUG`.
+*/
 
 /*
 ** FT_DEBUG_FD - file descriptor for debug messages
@@ -37,13 +47,21 @@
 ** @...: format arguments
 */
 void	ft_debug(const char *file, int line, const char *fmt, ...);
-# define FT_DEBUG(fmt, ...) ft_debug(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+# ifdef FT_FEATURE_DEBUG
+#  define FT_DEBUG(fmt, ...) ft_debug(__FILE__, __LINE__, fmt, ##__VA_ARGS__)
+# else
+#  define FT_DEBUG(fmt, ...)
+# endif
 
 /*
 ** FT_ASSERT - exit program if a condition is not met
 ** @expr: expression to check
 */
 void	ft_assert(const char *file, int line, const char *expr);
-# define FT_ASSERT(expr) ft_assert(__FILE__, __LINE__, (expr) ? NULL : #expr)
+# ifdef FT_FEATURE_DEBUG
+#  define FT_ASSERT(expr) ft_assert(__FILE__, __LINE__, (expr) ? NULL : #expr)
+# else
+#  define FT_ASSERT(expr)
+# endif
 
 #endif
