53c5b6389a0abd7205d4006a7b3a877bdb29fe2e
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-05-03 23:25:46 +0000

    Unclutter *printf functions

--- srcs/printf/ft_vfprintf.c
+++ srcs/printf/ft_vfprintf.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/08 15:50:26 by jguyon            #+#    #+#             */
-/*   Updated: 2017/03/29 18:50:20 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/05/03 23:24:04 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,67 @@
 #include "priv/pf_parse.h"
 #include "ft_strings.h"
 #include "ft_debug.h"
+#include <limits.h>
+#include <stdint.h>
 
-int				ft_vfprintf(t_stream *stream, const char *format, va_list args)
+static int	write_literal(t_stream *stm, const char *format, size_t size)
+{
+	size_t	count;
+
+	count = ft_fwrite(format, 1, size, stm);
+	if (count > INT_MAX)
+		return (-1);
+	return (count);
+}
+
+static int	write_conv(t_stream *stm, size_t count,
+				const char **format, va_list args)
+{
+	size_t		conv_count;
+	t_pf_info	info;
+	int			res;
+
+	conv_count = 0;
+	if (**format == PF_CONV_PREFIX)
+	{
+		conv_count = ft_fwrite(*format, 1, 1, stm);
+		++(*format);
+	}
+	else if ((*format = pf_parse_info(*format, &info, count, args)))
+	{
+		if ((res = pf_convert(stm, &info, args)) < 0)
+			return (-1);
+		conv_count = res;
+	}
+	if (!(*format) || ft_ferror(stm) || conv_count > INT_MAX)
+		return (-1);
+	return (conv_count);
+}
+
+int			ft_vfprintf(t_stream *stream, const char *format, va_list args)
 {
 	size_t		count;
 	int			res;
 	char		*next;
-	t_pf_info	info;
 
 	FT_ASSERT(stream != NULL);
 	FT_ASSERT(format != NULL);
-	if (!format || ft_ferror(stream))
+	if (ft_ferror(stream))
 		return (-1);
 	count = 0;
 	while ((next = ft_strchrnul(format, PF_CONV_PREFIX)))
 	{
-		count += (res = ft_fwrite(format, 1, next - format, stream));
-		if (res != next - format || !(*(format = next)))
+		if ((res = write_literal(stream, format, next - format)) < 0
+			|| (size_t)(INT_MAX - res) < count)
+			return (-1);
+		count += res;
+		if (res != next - format || !(*next))
 			break ;
-		if (*(++format) == PF_CONV_PREFIX)
-			count += ft_fwrite(format++, 1, 1, stream);
-		else if ((format = pf_parse_info(format, &info, count, args)))
-			count += (res = pf_convert(stream, &info, args));
-		if (res < 0 || !format || ft_ferror(stream))
-			break ;
+		format = next + 1;
+		if ((res = write_conv(stream, count, &format, args)) < 0
+			|| (size_t)(INT_MAX - res) < count)
+			return (-1);
+		count += res;
 	}
-	if (res < 0 || !format || ft_ferror(stream))
-		return (-1);
 	return (count);
 }
--- srcs/printf/ft_vsnprintf.c
+++ srcs/printf/ft_vsnprintf.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/15 16:02:31 by jguyon            #+#    #+#             */
-/*   Updated: 2017/03/29 18:49:37 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/05/03 20:47:59 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,12 @@
 
 static t_stream_funs	g_noop_funs;
 
-int						ft_vsnprintf(char *str, size_t size,
-							const char *format, va_list args)
+static int	count_chars(const char *format, va_list args)
 {
 	t_stream	*stm;
 	int			count;
 	va_list		cpy;
 
-	FT_ASSERT(format != NULL);
-	if (!str && size > 0)
-		return (-1);
 	if (!(stm = ft_fopencookie(NULL, "w", g_noop_funs)))
 		return (-1);
 	ft_setvbuf(stm, NULL, FT_IONBF, 0);
@@ -33,13 +29,26 @@ int						ft_vsnprintf(char *str, size_t size,
 	va_end(cpy);
 	if (ft_fclose(stm) || count < 0)
 		return (-1);
-	if (!str)
+	return (count);
+}
+
+int			ft_vsnprintf(char *str, size_t size,
+				const char *format, va_list args)
+{
+	t_stream	*stm;
+	int			count;
+
+	FT_ASSERT(format != NULL);
+	FT_ASSERT(str != NULL || size == 0);
+	count = count_chars(format, args);
+	if (!str || count < 0)
 		return (count);
 	if (!(stm = ft_fmemopen(str, size, "w")))
 		return (-1);
 	ft_setvbuf(stm, NULL, FT_IONBF, 0);
 	ft_vfprintf(stm, format, args);
-	ft_fclose(stm);
+	if (ft_fclose(stm))
+		return (-1);
 	str[(size_t)count >= size ? size - 1 : count] = '\0';
 	return (count);
 }
